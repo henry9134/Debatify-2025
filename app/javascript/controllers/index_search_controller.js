bd57cac4c4c450538da67326212030602e05ae19
Dylan Mayoral <dylanmayoral@gmail.com>
2024-11-27 17:15:22 +0900

    made more changes

--- app/javascript/controllers/index_search_controller.js
+++ app/javascript/controllers/index_search_controller.js
@@ -1,24 +1,22 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="search"
+
 export default class extends Controller {
   static targets = ["input", "results"]
 
   connect() {
-    console.log("Search controller connected.");
+    console.log("Index Search controller connected.");
   }
 
-  // Triggered on keyup
   search(event) {
     const query = this.inputTarget.value;
 
-    // Send the query to the server using fetch
     fetch(`/topics?query=${encodeURIComponent(query)}`, {
-      headers: { 'Accept': 'text/plain' } // This tells Rails to send the `format.text` response
+      headers: { 'Accept': 'text/plain' }
     })
       .then(response => response.text())
       .then(html => {
-        this.resultsTarget.innerHTML = html; // Update the results container
+        this.resultsTarget.innerHTML = html;
       });
   }
 }
