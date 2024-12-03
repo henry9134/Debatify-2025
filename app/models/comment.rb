62414aef20dcf10bcdd862f73400c85ab164e7b3
Dylan Mayoral <dylanmayoral@gmail.com>
2024-12-03 16:07:14 +0900

    replies of replies

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,7 +5,7 @@ class Comment < ApplicationRecord
   has_many :comments, foreign_key: :parent_id
   has_many :votes, dependent: :destroy
 
-  validates :content, presence: true, length: { minimum: 10 }
+  validates :content, presence: true, length: { minimum: 3 }
 
   enum status: { against: "against", neutral: "neutral", for: "for" }
 end
