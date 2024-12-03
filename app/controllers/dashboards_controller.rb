25c5e31cd41c116060862c33dc8ba065f9cf0247
Henry Santiago <hjsantiago13@gmail.com>
2024-12-03 13:02:48 +0900

    styling

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,10 +5,12 @@ class DashboardsController < ApplicationController
     @topics = @user.favorited_topics.sample(rand(3..5))
     @users = User.all
 
-    @point_topics_user = @user.topics.count
-    @point_comments_user = @user.comments.count * 2
-    @point_comments_upvote = Vote.where(comment_id: @user.comments.pluck(:id)).count * 5
-    @user.point = @point_topics_user + @point_comments_user + @point_comments_upvote
+    @users.each do |user|
+      point_topics_user = user.topics.count
+      point_comments_user = user.comments.count * 2
+      point_comments_upvote = Vote.where(comment_id: user.comments.pluck(:id)).count * 5
+      user.point = point_topics_user + point_comments_user + point_comments_upvote
+      user.save
+    end
   end
 end
- 
